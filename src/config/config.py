ba72bc234abba05bf5f5d5a1ba58eda49aa416f3
Manuel Rottschaefer <manuel.rottschaefer@tum.de>
2024-11-29 16:27:39 +0100

    Add static and configuration data

--- src/config/config.py
+++
@@ -1,6 +0,0 @@
-"""This module handles all config related tasks."""
-
-import json
-
-with open('src/config/locker_config.json', 'r', encoding='utf-8') as config_file:
-    locker_config = json.load(config_file)
